bbc00ea6bbd1993caf7f60bad7d6adaeedae1d9b
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-08-17 22:15:31 +0200

    tried building the form

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [days, setDays] = useState(null);
+  const [month, setMonth] = useState(null);
+  const [year, setYear] = useState(null);
+  const [daysInMonth, setDaysInMonth] = useState(null);
+
+  const date = new Date()
+
+  useEffect(() => {
+    setDaysInMonth(new Date(year, month, 0).getDate())
+    console.log(daysInMonth)
+  }, [year, month])
+
+
+  return (
+    <main>
+      <form>
+        <label>
+          DAY
+          <input
+            onChange={(e) => setDays(e.target.value)}
+            type="number"
+            name="day"
+            placeholder="DD"
+            value={days}
+            min={0}
+            max={daysInMonth}
+            required
+          />
+        </label>
+        <label>
+          MONTH
+          <input
+          onChange={(e) => setMonth(e.target.value)}
+            type="number"
+            name="day"
+            placeholder="MM"
+            value={month}
+            min={0}
+            max={12}
+            required
+          />
+        </label>
+        <label>
+          YEAR
+          <input
+          onChange={(e) => setYear(e.target.value)}
+            type="number"
+            name="day"
+            placeholder="YYYY"
+            value={year}
+            min={0}
+            max={date.getFullYear()}
+            required
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div className="results-cnt">
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
